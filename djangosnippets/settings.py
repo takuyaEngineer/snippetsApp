249a46d0bccb4064a3fda70efbf7761d89c8c73f
takuyaEngineer <engineer.memorandum@gmail.com>
2023-12-28 15:47:47 +0900

    fix settings

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -25,13 +25,12 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=san+fy9*eijr3#yys-@p+q)0=(v7w&!qi2x(l^ak-_8g62s-)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 # Application definition
 
